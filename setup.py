987d6c4eb5a40a8f6d927d8fe26ac232e11467c1
Antoine Duchêne <antoine.duchene@imio.be>
2026-08-03 09:28:17 +0200

    Back to development: 1.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='imio.omnia.assistant',
-    version='1.0',
+    version='1.1.dev0',
     description="AI chat assistant widget integrating iMio's Omnia API for Plone",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
